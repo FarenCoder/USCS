c86a66bc18fcd8258a19dff8be6ba99e029e8008
Rodolfo Moreno <43105457+RodolfoCMoreno@users.noreply.github.com>
2022-11-18 16:42:47 -0300

    bancoDeDados

---
+++ banco de dados/InsertsTabFilme.sql
@@ -0,0 +1,203 @@
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Batman: O Cavaleiro das Trevas', 152, 2008, 8.9, 'The Dark Knight');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('A Lista de Schindler', 195, 1993, 8.9, 'Schindlers List');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Pulp Fiction: Tempo de Violência', 154, 1994, 8.9, 'Pulp Fiction');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('O Senhor dos Anéis: O Retorno do Rei', 201, 2003, 8.9, 'The Lord of the Rings: The Return of the King');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Guerra nas Estrelas', 121, 1977, 8.7, 'Star Wars');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('O Império Contra-Ataca', 124, 1980, 8.8, 'Star Wars: Episode V - The Empire Strikes Back');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('O Retorno de Jedi', 131, 1983, 8.4, 'Star Wars: Episode VI - Return of the Jedi');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Star Wars: Episódio I - A Ameaça Fantasma', 136, 1999, 6.5, 'Star Wars: Episode I - The Phantom Menace');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Star Wars: Episódio II - Ataque dos Clones', 142, 2002, 6.7, 'Star Wars: Episode II - Attack of the Clones');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Star Wars: Episódio III - A Vingança dos Sith', 140, 2005, 7.7, 'Star Wars: Episode III - Revenge of the Sith');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Star Wars: Episódio VII - O Despertar da Força', 136, 2015, 8.8, 'Star Wars: The Force Awakens');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Clube da Luta', 139, 1999, 8.9, 'Fight Club');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('A Origem', 148, 2010, 8.8, 'Inception');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Matrix', 136, 1999, 8.7, 'The Matrix');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('O Silêncio dos Inocentes', 118, 1991, 8.6, 'The Silence of the Lambs');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Interestelar', 169, 2014, 8.6, 'Interstellar');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('O Resgate do Soldado Ryan', 169, 1998, 8.6, 'Saving Private Ryan');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('A Outra História Americana', 119, 1998, 8.6, 'American History X');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('De Volta para o Futuro', 116, 1985, 8.5, 'Back to the Future');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('De Volta para o Futuro Parte II', 108, 1989, 7.8, 'Back to the Future Part II');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('De Volta para o Futuro Parte III', 118, 1990, 7.4, 'Back to the Future Part III');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Amnésia', 113, 2000, 8.5, 'Memento');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Laranja Mecânica', 136, 1971, 8.4, 'A Clockwork Orange');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Toy Story', 81, 1995, 8.3, 'Toy Story');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Scarface', 170, 1983, 8.3, 'Scarface');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Seven: Os Sete Crimes', 127, 1995, 8.6, 'Se7en');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('WALL·E', 98, 2008, 8.4, 'WALL·E');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Bastardos Inglórios', 153, 2009, 8.3, 'Inglourious Basterds');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('O Lobo de Wall Street', 180, 2013, 8.2, 'The Wolf of Wall Street');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Uma Mente Brilhante', 135, 2001, 8.2, 'A Beautiful Mind');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Perdido em Marte', 144, 2015, 8.2, 'The Martian');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('O Sexto Sentido', 107, 1999, 8.2, 'The Sixth Sense');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Gladiador', 155, 2000, 8.5, 'Gladiator');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Waterworld - O Segredo das Águas', 135, 1995, 6.1, 'Waterworld');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Os 12 Macacos', 129, 1995, 8.1, 'Twelve Monkeys');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Coração Valente', 178, 1995, 8.4, 'Braveheart');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Os Bad Boys', 118, 1995, 6.8, 'Bad Boys');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Mortal Kombat', 101, 1995, 5.8, 'Mortal Kombat');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('A Experiência', 108, 1995, 5.8, 'Species');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Duro de Matar - A Vingança', 131, 1995, 7.6, 'Die Hard: With a Vengeance');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('O Juiz', 96, 1995, 5.4, 'Judge Dredd');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Mentes Perigosas', 99, 1995, 6.4, 'Dangerous Minds');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Duro de Matar', 131, 1988, 8.2, 'Die Hard');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Duro de Matar 2', 124, 1990, 7.1, 'Die Hard 2');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Duro de Matar: Um Bom Dia para Morrer', 98, 2013, 5.3, 'A Good Day to Die Hard');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Jurassic Park: O Parque dos Dinossauros', 127, 1993, 8.1, 'Jurassic Park');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('O Mundo Perdido: Jurassic Park', 129, 1997, 6.5, 'The Lost World: Jurassic Park');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Jurassic Park III', 92, 2001, 5.9, 'Jurassic Park III');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Jurassic World: O Mundo dos Dinossauros', 124, 2015, 7.1, 'Jurassic World');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Harry Potter e a Pedra Filosofal', 152, 2001, 7.5, 'Harry Potter and the Sorcerers Stone');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Harry Potter e a Câmara Secreta', 161, 2002, 7.4, 'Harry Potter and the Chamber of Secrets');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Harry Potter e o Prisioneiro de Azkaban', 142, 2004, 7.8, 'Harry Potter and the Prisoner of Azkaban');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Harry Potter e o Cálice de Fogo', 157, 2005, 7.6, 'Harry Potter and the Goblet of Fire');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Harry Potter e a Ordem da Fênix', 138, 2007, 7.5, 'Harry Potter and the Order of the Phoenix');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Harry Potter e o Enigma do Príncipe', 153, 2009, 7.5, 'Harry Potter and the Half-Blood Prince');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Harry Potter e as Relíquias da Morte: Parte 1', 146, 2010, 7.7, 'Harry Potter and the Deathly Hallows: Part 1');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Harry Potter e as Relíquias da Morte: Parte 2', 130, 2011, 8.1, 'Harry Potter and the Deathly Hallows: Part 2');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Super-Heróis: A Liga da Injustiça', Null, 2008, 1.9, 'Disaster Movie');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Zombie Nation', Null, 2005, 2.1, 'Zombie Nation');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('House of the Dead: O Filme', Null, 2003, 2.0, 'House of the Dead');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Alone in the Dark - O Despertar do Mal', Null, 2005, 2.3, 'Alone in the Dark');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Velozes e Furiosos', 106, 2001, 6.7, 'The Fast and the Furious');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('+Velozes +Furiosos', 107, 2003, 5.8, '2 Fast 2 Furious');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Velozes e Furiosos - Desafio em Tóquio', 104, 2006, 6.0, 'The Fast and the Furious: Tokyo Drift');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Velozes e Furiosos 4', 107, 2009, 6.6, 'Fast & Furious');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Velozes & Furiosos 5: Operação Rio', 131, 2011, 7.3, 'Fast Five');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Velozes & Furiosos 6', 130, 2013, 7.2, 'Furious 6');
+
+INSERT INTO filme (Nome, Duracao, AnoLancamento, AvaliacaoFilme, NomeOriginal) 
+VALUES ('Velozes & Furiosos 7', 137, 2015, 7.3, 'Furious Seven');
